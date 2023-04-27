117d8f19cfec834553bc42750d9adba668ec0fe6
gyeong deok <107936957+littleduck1219@users.noreply.github.com>
2023-04-27 23:46:42 +0900

    [Bronze V] Title: 사분면 고르기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,11 @@
+x,y = map(int, (input(), input()))
+if x > 0:
+    if y > 0:
+        print(1)
+    elif y < 0:
+        print(4)
+elif x < 0:
+    if y > 0:
+        print(2)
+    elif y < 0:
+        print(3)
