b100d070237b0c0fa19643c1fcab7afc49bbe28b
gyeong deok <107936957+littleduck1219@users.noreply.github.com>
2023-05-01 18:50:58 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -0,0 +1,16 @@
+def stars(num):
+    for i in range(1, num+1):
+        left_spaces = num - i
+        stars = i * 2 - 1
+        line = " " * left_spaces + "*" * stars
+        print(line)
+
+    for i in range(num-1, 0, -1):
+        left_spaces = num - i
+        stars = i * 2 - 1
+        line = " " * left_spaces + "*" * stars
+        print(line)
+
+
+num = int(input())
+stars(num)
