2caa6a39004ce1be22e2709d1993bd956453127e
gyeong deok <107936957+littleduck1219@users.noreply.github.com>
2023-05-06 01:38:22 +0900

    [Silver V] Title: 색종이, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,13 @@
+array = [[0 for _ in range(101)] for _ in range(101)]
+n = int(input())
+for _ in range(n):
+    x, y = list(map(int, input().split()))
+    
+    for i in range(x, x+10):
+        for j in range(y, y+10):
+            array[i][j] = 1
+ 
+result = 0
+for i in array:
+    result += i.count(1)
+print(result)
