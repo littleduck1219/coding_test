c9b9291f0ce99b1a2885a06a6f0e2149bd835034
gyeong deok <107936957+littleduck1219@users.noreply.github.com>
2023-06-18 11:29:06 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -1,14 +1,8 @@
 function solution(a, b) {
-	result = 0;
-	if (a < b) {
-		for (i = a; i <= b; i++) {
-			result += i;
-		}
-	} else {
-		for (i = b; i <= a; i++) {
-			result += i;
-		}
-	}
-
-	return result;
+    let answer = 0;
+    if (a > b) [a, b] = [b, a];
+    for (let i = a; i <= b; i++) {
+        answer += i;
+    }
+    return answer;
 }
