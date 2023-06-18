856cc5531cc207dab0dbf45a96e2a831f131d537
gyeong deok <107936957+littleduck1219@users.noreply.github.com>
2023-06-18 10:55:16 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -1,8 +1,7 @@
 function solution(num) {
-	let result = Math.abs(num % 2);
-	if (result == 1) {
-		return "Odd";
-	} else {
-		return "Even";
-	}
+    var answer = '';
+    if (num%2==0) {
+        return "Even"
+    }
+    return "Odd"
 }
