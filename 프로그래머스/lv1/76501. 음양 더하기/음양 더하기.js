12832043f3f5fe9298cf7a9b8fc24b449c7f3d98
gyeong deok <107936957+littleduck1219@users.noreply.github.com>
2023-06-19 03:22:21 +0900

    [level 1] Title: 음양 더하기, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,13 +1,7 @@
 function solution(absolutes, signs) {
-  var answer = 0;
-  for (i = 0; i < absolutes.length; i++) {
-      if (signs[i] == true) {
-          answer += absolutes[i];
-      }
-      else {
-          answer -= absolutes[i];
-      }
-  }
-  
-  return answer;
+    let answer = 0;
+    for (let i = 0; i < absolutes.length; i++) {
+        signs[i] ? answer += absolutes[i] : answer -= absolutes[i]
+    }
+    return answer
 }
