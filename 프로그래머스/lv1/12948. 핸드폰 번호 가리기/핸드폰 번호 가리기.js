f40ac9ac0a5479fe3c897a497e68265b016d7f2c
gyeong deok <107936957+littleduck1219@users.noreply.github.com>
2023-06-19 09:20:08 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,5 +1,4 @@
 function solution(phone_number) {
-	let last_num = phone_number.slice(-4);
-	let star = "*".repeat(phone_number.length - 4);
-	return `${star}${last_num}`;
+    let star = "*".repeat(phone_number.length-4) 
+    return `${star}${phone_number.slice(-4)}`
 }
