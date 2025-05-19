325db40fb4b087331725a1d605182ecd177b49b8
littleduck <107936957+littleduck1219@users.noreply.github.com>
2025-05-19 23:11:53 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.js
@@ -0,0 +1,9 @@
+function solution(num_list, n) {
+    // const answer = [];
+    // for (let i = n - 1; i < num_list.length; i++) {
+    //     answer.push(num_list[i])
+    // }
+    // return answer
+    
+    return num_list.slice( n - 1);
+}
