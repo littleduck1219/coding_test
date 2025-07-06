b67d546e39e4cd0091181cae14f3487558e54a61
littleduck <107936957+littleduck1219@users.noreply.github.com>
2025-07-07 00:45:15 +0900

    [level 0] Title: 2의 영역, Time: 27.45 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.js
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.js
@@ -1,10 +1,35 @@
-function solution(arr) {
-    const firstTwoIndex = arr.indexOf(2);
-    const lastTwoIndex = arr.lastIndexOf(2);
+// function solution(arr) {
+//     const firstTwoIndex = arr.indexOf(2);
+//     const lastTwoIndex = arr.lastIndexOf(2);
     
-    if (firstTwoIndex === -1) {
+//     if (firstTwoIndex === -1) {
+//         return [-1];
+//     }
+    
+//     return arr.slice(firstTwoIndex, lastTwoIndex + 1);
+// }
+
+function solution(arr) {
+    let firstIndex = -1;
+    let lastIndex = -1;
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 2) {
+            firstIndex = i
+            break;
+        }
+    }
+    
+    if (firstIndex === -1) {
         return [-1];
     }
     
-    return arr.slice(firstTwoIndex, lastTwoIndex + 1);
+    for (let i = arr.length - 1; i >=0; i--) {
+        if (arr[i] === 2) {
+            lastIndex = i;
+            break;
+        }
+    }
+    
+    return arr.slice(firstIndex, lastIndex + 1);
 }
--- 프로그래머스/0/181894. 2의 영역/README.md
+++ 프로그래머스/0/181894. 2의 영역/README.md
@@ -1,10 +1,10 @@
 # [level 0] 2의 영역 - 181894 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181894?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181894) 
 
 ### 성능 요약
 
-메모리: 42.2 MB, 시간: 0.40 ms
+메모리: 38.5 MB, 시간: 27.45 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 07일 00:38:54
+2025년 07월 07일 00:45:13
 
 ### 문제 설명
 
