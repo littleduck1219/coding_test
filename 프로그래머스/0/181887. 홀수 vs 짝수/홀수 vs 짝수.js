ba4f3ef9629028f45f625c1b3a3e20e165b4398e
littleduck <107936957+littleduck1219@users.noreply.github.com>
2025-07-18 19:10:30 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.15 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -1,27 +1,15 @@
 function solution(num_list) {
-    let a = [];
-    let b = [];
+    let odd = 0;
+    let even = 0;
     
     for (let i = 0; i < num_list.length; i++) {
         if (i % 2 === 0) {
-            a.push(num_list[i])
+            even += num_list[i]
         } else {
-            b.push(num_list[i])
+            odd += num_list[i]
         }
     }
     
-    const sumA = a.reduce((acc, cur) => {
-        return acc + cur
-    }, 0)
-    const sumB = b.reduce((acc, cur) => {
-        return acc + cur
-    }, 0)
-    
-    console.log("sumA",sumA)
-    console.log("sumB",sumB)
-    
-    const big = sumA > sumB ? sumA : sumB;
-    
-    return big;
+    return odd > even ? odd : even;
 }
     
