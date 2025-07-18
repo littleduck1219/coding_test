2e33aabd0bea930d87ed37e55620b89bd8c127dc
littleduck <107936957+littleduck1219@users.noreply.github.com>
2025-07-18 21:18:16 +0900

    [level 0] Title: 5명씩, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.js
+++ 프로그래머스/0/181886. 5명씩/5명씩.js
@@ -1,10 +1,14 @@
 function solution(names) {
-    let answer = [];
+//     let answer = [];
     
-    for (let i = 0; i < names.length; i++) {
-        if (i % 5 === 0) {
-            answer.push(names[i])
-        }
-    }
-    return answer;
+//     for (let i = 0; i < names.length; i++) {
+//         if (i % 5 === 0) {
+//             answer.push(names[i])
+//         }
+//     }
+//     return answer;
+    
+     // return Array.from({ length: Math.ceil(names.length / 5) }, (_, i) => names[i * 5]);
+    
+    return Array.from({length: Math.ceil(names.length/5)}, (_, i) => names[i * 5])
 }
