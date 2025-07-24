0ed295c48fd2729e8bc5e8b1a5822ebf46f85bb8
littleduck <107936957+littleduck1219@users.noreply.github.com>
2025-07-24 23:20:16 +0900

    [level 0] Title: 1로 만들기, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
@@ -1,19 +1,4 @@
 function solution(num_list) {
-    let count = 0;
-    
-    for (let i = 0; i < num_list.length; i++) {
-        let currentNum = num_list[i];
-        
-        while (currentNum !== 1) {
-            if (currentNum % 2 === 0) {
-                currentNum /= 2;
-            } else {
-                currentNum = (currentNum - 1) / 2;
-            }
-            
-            count++;
-        }
-    }
-    
-    return count;
+    var answer = 0;
+    return answer;
 }
