7265ac3da864c0267eb82bfba5d82f6930aaeea3
littleduck <107936957+littleduck1219@users.noreply.github.com>
2025-07-24 23:56:24 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -0,0 +1,20 @@
+function solution(num_list) {
+   
+    if (num_list.length >= 11) {
+        let answer = 0;
+        for (let i = 0; i < num_list.length; i++) {
+            answer += num_list[i];
+        }
+        
+        return answer;
+    } else if (num_list.length <= 10) {
+         let answer = 1;
+        for (let i = 0; i < num_list.length; i++) {
+            answer *= num_list[i];
+        }
+        
+        return answer;
+    }
+    
+    
+}
