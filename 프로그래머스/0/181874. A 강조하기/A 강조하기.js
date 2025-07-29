5e59c09730aca99a3e3ffb2f9349d1b6695c3886
littleduck <107936957+littleduck1219@users.noreply.github.com>
2025-07-30 08:03:20 +0900

    [level 0] Title: A 강조하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.js
@@ -1,15 +1,17 @@
 function solution(myString) {
-    let stringArr = myString.split("");
+//     let stringArr = myString.split("");
     
-    for (let i = 0; i < stringArr.length; i++) {
-        if ( stringArr[i] === 'a') {
+//     for (let i = 0; i < stringArr.length; i++) {
+//         if ( stringArr[i] === 'a') {
             
         
-            stringArr[i] = 'A'
-        } else if (stringArr[i] !== 'A' && stringArr[i] === stringArr[i].toUpperCase()) {
-            stringArr[i] = stringArr[i].toLowerCase();
-        }
-    }
+//             stringArr[i] = 'A'
+//         } else if (stringArr[i] !== 'A' && stringArr[i] === stringArr[i].toUpperCase()) {
+//             stringArr[i] = stringArr[i].toLowerCase();
+//         }
+//     }
     
-    return stringArr.join("")
-    }
+//     return stringArr.join("")
+    
+    return myString.toLowerCase().replaceAll('a', 'A');    
+}
