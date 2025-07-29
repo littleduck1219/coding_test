a09a1f37c467766206f57bae356c72355b091433
littleduck <107936957+littleduck1219@users.noreply.github.com>
2025-07-30 08:01:34 +0900

    [level 0] Title: A 강조하기, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.js
@@ -0,0 +1,15 @@
+function solution(myString) {
+    let stringArr = myString.split("");
+    
+    for (let i = 0; i < stringArr.length; i++) {
+        if ( stringArr[i] === 'a') {
+            
+        
+            stringArr[i] = 'A'
+        } else if (stringArr[i] !== 'A' && stringArr[i] === stringArr[i].toUpperCase()) {
+            stringArr[i] = stringArr[i].toLowerCase();
+        }
+    }
+    
+    return stringArr.join("")
+    }
