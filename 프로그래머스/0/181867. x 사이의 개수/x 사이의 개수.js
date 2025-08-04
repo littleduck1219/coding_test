83acb946a7fc665284c456e9651f0aebfc012261
littleduck <107936957+littleduck1219@users.noreply.github.com>
2025-08-05 08:06:13 +0900

    [level 0] Title: x 사이의 개수, Time: 31.70 ms, Memory: 38.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.js
@@ -0,0 +1,11 @@
+function solution(myString) {
+    var answer = [];
+    let split = myString.split("x");
+    console.log(split)
+    
+    for (let i = 0; i < split.length; i++) {
+        answer.push(split[i].length)
+    }
+    
+    return answer;
+}
