49d12704cf6ab8e93644d395b788b42ac75fd1ac
littleduck <107936957+littleduck1219@users.noreply.github.com>
2025-10-23 23:27:30 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,8 @@
+function solution(a, b) {
+    const odd = (num) => num % 2 !== 0
+    const pow = (num) => Math.pow(num, 2)
+    
+    if (odd(a) && odd(b)) return pow(a) + pow(b);
+    if (odd(a) || odd(b)) return 2 * (a + b);
+    if (!odd(a) && !odd(b)) return Math.abs(a - b)
+}
