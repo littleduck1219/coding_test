efc1409a19c396b5e0af442c867f9390aa2fd562
littleduck <107936957+littleduck1219@users.noreply.github.com>
2025-10-23 21:31:23 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.js
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.js
@@ -1,3 +1,12 @@
 function solution(my_string, target) {
-    return my_string.includes(target) ? 1 : 0
+    // return my_string.includes(target) ? 1 : 0
+    for (let i = 0; i <= my_string.length; i++) {
+        const sub_string = my_string.slice(i, i + target.length);
+        
+        if (sub_string === target) {
+            return 1
+        }
+    }
+    
+    return 0
 }
